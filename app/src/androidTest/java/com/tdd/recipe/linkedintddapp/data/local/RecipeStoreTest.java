4144947089eb961bfc88e469712204f2a749a771
Philipp Bugaevskii <philippdevel@gmail.com>
2018-02-21 17:36:20 +0200

    Update: RecipeStore class for providing access to files store.

---
+++ app/src/androidTest/java/com/tdd/recipe/linkedintddapp/data/local/RecipeStoreTest.java
@@ -0,0 +1,20 @@
+package com.tdd.recipe.linkedintddapp.data.local;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RecipeStoreTest {
+
+    @Test
+    public void nullDirectory() throws Exception {
+        Context context = InstrumentationRegistry.getTargetContext();
+        RecipeStore store = new RecipeStore(context, null);
+        assertNotNull(store);
+        assertNotNull(store.recipes);
+        assertEquals(0, store.recipes.size());
+    }
+}
