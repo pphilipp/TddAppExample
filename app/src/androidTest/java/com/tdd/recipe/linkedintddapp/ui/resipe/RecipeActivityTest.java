5681c0eae9eaebd898e746c8e1cf2ab908ecf2eb
Philipp Bugaevskii <philippdevel@gmail.com>
2018-03-12 14:09:46 +0200

    Update: clean code

--- app/src/androidTest/java/com/tdd/recipe/linkedintddapp/ui/resipe/RecipeActivityTest.java
+++ app/src/androidTest/java/com/tdd/recipe/linkedintddapp/ui/resipe/RecipeActivityTest.java
@@ -1,27 +1,10 @@
 package com.tdd.recipe.linkedintddapp.ui.resipe;
 
-import android.content.Intent;
-import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
-
 import com.tdd.recipe.linkedintddapp.R;
-import com.tdd.recipe.linkedintddapp.data.local.InMemoryFavorites;
-import com.tdd.recipe.linkedintddapp.injection.TestRecipeApplication;
 import com.tdd.recipe.linkedintddapp.test.RecipeRobot;
-
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.isSelected;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.core.IsNot.not;
-
 
 public class RecipeActivityTest {
     private final String TEST_ID_MILK = "milk";
